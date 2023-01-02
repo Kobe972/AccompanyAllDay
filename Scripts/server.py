836d32973580bb47cb2e85d2349a80c7087a5f04
Kobe972 <xuyichang@mail.ustc.edu.cn>
2023-01-02 23:55:39 +0800

    first version

---
+++ Scripts/server.py
@@ -0,0 +1,88 @@
+from flask import *
+import datetime
+import json
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+status1 = [{"status": "想你", "time": 0}]
+status2 = [{"status": "想你", "time": 0}]
+
+@app.route("/uploadPhoto", methods=["POST"])
+def upload_photo():
+    id = request.form.get("id")
+    file = request.files.get("file")
+    if file and id:
+        # Create the directory for storing the photos if it doesn't exist
+        if not os.path.exists("photos"):
+            os.makedirs("photos")
+        # Sanitize the file name and construct the file path
+        filename = secure_filename(file.filename)
+        # Get the file extension
+        _, file_extension = os.path.splitext(filename)
+        # Construct the file path
+        filepath = os.path.join("photos", id + file_extension)
+        # Delete any existing files with the same name (ignoring the file extension)
+        for f in os.listdir("photos"):
+            if f.startswith(id):
+                os.remove(os.path.join("photos", f))
+        # Save the file to the file path
+        file.save(filepath)
+        return "Success"
+    return "Error"
+
+@app.route("/deletePhoto", methods=["POST"])
+def delete_photo():
+    id = request.form.get("id")
+    for f in os.listdir("photos"):
+        if f.startswith(id):
+            filepath = os.path.join("photos", f)
+            os.remove(filepath)
+    return "Success"
+
+@app.route("/getPhoto")
+def get_photo():
+    id = request.args.get("id")
+    for f in os.listdir("photos"):
+        if f.startswith(id):
+            filepath = os.path.join("photos", f)
+            mimetype = 'image/' + f.rsplit('.', 1)[1]
+            return send_file(filepath, mimetype=mimetype)
+    return "fail"
+
+
+@app.route("/getStatus1")
+def get_status1():
+    return json.dumps(status1,ensure_ascii=False)
+
+@app.route("/getStatus2")
+def get_status2():
+    return json.dumps(status2,ensure_ascii=False)
+
+@app.route("/setStatus", methods=["POST"])
+def set_status():
+    id = request.form.get("id")
+    status = request.form.get("status")
+    time = request.form.get("time")
+
+    if id == "1":
+        status1.append({"status": status, "time": int(time)})
+        return json.dumps(status1,ensure_ascii=False)
+    elif id == "2":
+        status2.append({"status": status, "time": int(time)})
+        return json.dumps(status2,ensure_ascii=False)
+    else:
+        return "Invalid id"
+@app.before_request
+def reset_statuses():
+    # 获取当前时间
+    current_time = datetime.datetime.now()
+    # 判断当前时间是否是每天的0点
+    if current_time.hour == 0 and current_time.minute == 0:
+        status1 = [{"status": "想你", "time": 0}]
+        status2 = [{"status": "想你", "time": 0}]
+
+
+if __name__ == "__main__":
+    app.run(port=7002)
